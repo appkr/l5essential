82ba287f37db0c4dd3f50ef6cec2481625a8677e
Appkr <juwonkim@me.com>
2015-11-10 17:42:52 +0900

    13-restful-resource-controller

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        return '[' . __METHOD__ . '] ' . 'respond the index page';
+    }
+
+    public function create()
+    {
+        return '[' . __METHOD__ . '] ' . 'respond a create form';
+    }
+
+    public function store(Request $request)
+    {
+        return '[' . __METHOD__ . '] ' . 'validate the form data from the create form and create a new instance';
+    }
+
+    public function show($id)
+    {
+        return '[' . __METHOD__ . '] ' . 'respond an instance having id of ' . $id;
+    }
+
+    public function edit($id)
+    {
+        return '[' . __METHOD__ . '] ' . 'respond an edit form for id of ' . $id;
+    }
+
+    public function update(Request $request, $id)
+    {
+        return '[' . __METHOD__ . '] ' . 'validate the form data from the edit form and update the resource having id of ' . $id;
+    }
+
+    public function destroy($id)
+    {
+        return '[' . __METHOD__ . '] ' . 'delete resource ' . $id;
+    }
+}
