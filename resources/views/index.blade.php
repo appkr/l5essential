439d984a97d0b2193e93073af0ffeee67f736184
Appkr <juwonkim@me.com>
2015-11-10 15:53:28 +0900

    6-blade-101

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,20 @@
-<p>
-    {{ $greeting }} {{ $name or '' }}. Welcome Back~
-</p>
+{{--@foreach--}}
+<ul>
+    @foreach($items as $item)
+        <li>{{ $item }}</li>
+    @endforeach
+</ul>
+
+{{--@if--}}
+{{--@if($itemCount = count($items))--}}
+    {{--<p>There are {{ $itemCount }} items !</p>--}}
+{{--@else--}}
+    {{--<p>There is no item !</p>--}}
+{{--@endif--}}
+
+{{--@forelse--}}
+{{--@forelse($items as $item)--}}
+    {{--<p>The item is {{ $item }}</p>--}}
+{{--@empty--}}
+    {{--<p>There is no item !</p>--}}
+{{--@endforelse--}}
