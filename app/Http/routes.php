e1fc601f45d1b931e296acfce67fedee919811fd
Appkr <juwonkim@me.com>
2015-11-10 17:56:28 +0900

    15-nested-resources

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,13 +1,3 @@
 <?php
 
-Route::get('posts', [
-    'as'   => 'posts.index',
-    'uses' => 'PostsController@index'
-]);
-
-//Route::get('posts', [
-//    'as' => 'posts.index',
-//    function() {
-//        return view('posts.index');
-//    }
-//]);
+Route::resource('posts.comments', 'PostCommentController');
