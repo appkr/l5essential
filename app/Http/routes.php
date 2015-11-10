7c6c0b73d3fb1310668dfd37aec02e22dbfa6d3c
Appkr <juwonkim@me.com>
2015-11-10 17:48:02 +0900

    14-named-routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,3 +1,13 @@
 <?php
 
-Route::resource('posts', 'PostsController');
+Route::get('posts', [
+    'as'   => 'posts.index',
+    'uses' => 'PostsController@index'
+]);
+
+//Route::get('posts', [
+//    'as' => 'posts.index',
+//    function() {
+//        return view('posts.index');
+//    }
+//]);
