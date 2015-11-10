065f3e1148767dc99a1e1303c917b771ab3ad1cb
Appkr <juwonkim@me.com>
2015-11-10 15:53:28 +0900

    6-blade-101

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,33 +1,10 @@
 <?php
 
-// Data binding using with() method
 Route::get('/', function () {
-    $greeting = 'Hello';
+    $items = [
+        'Apple',
+        'Banana'
+    ];
 
-    return view('index')->with('greeting', $greeting);
+    return view('index', compact('items'));
 });
-
-//// Binding more than two variables
-//Route::get('/', function () {
-//    return view('index')->with([
-//        'greeting' => 'Good morning ^^/',
-//        'name'     => 'Appkr'
-//    ]);
-//});
-//
-//// Binding data as a second argument of view() helper
-//Route::get('/', function () {
-//    return view('index', [
-//        'greeting' => 'Ola~',
-//        'name'     => 'Laravelians'
-//    ]);
-//});
-//
-//// Binding data as a property of View instance
-//Route::get('/', function () {
-//    $view = view('index');
-//    $view->greeting = "Hey~ What's up";
-//    $view->name = 'everyone';
-//
-//    return $view;
-//});
