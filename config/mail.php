685a3b73f4c73d5b38762d5462d63657811ce718
Appkr <juwonkim@me.com>
2015-11-10 23:10:02 +0900

    21-mail

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'john@example.com', 'name' => 'John Doe'],
 
     /*
     |--------------------------------------------------------------------------
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,5 @@
+<h1>{{ $title }} {{ $user->name }}</h1>
+<hr/>
+<p>{{ $body }}</p>
+<hr/>
+<footer>Mail from {{ config('app.url') }}</footer>
