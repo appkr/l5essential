31075b88140d7dd3ff23fcf55b7d196227e5f437
Appkr <juwonkim@me.com>
2015-11-10 17:17:26 +0900

    11-migration

---
+++ database/migrations/2015_11_10_080609_create_authors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function($table) {
+            $table->increments('id');
+            $table->string('email', 255);
+            $table->string('password', 60);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
