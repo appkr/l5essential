812380832a0414f585da7b2d89b2feca380855b3
Appkr <juwonkim@me.com>
2015-11-10 21:54:38 +0900

    19-seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title'   => $faker->sentence,
+        'body'    => $faker->paragraph,
+        'user_id' => App\User::all()->random()->id
+    ];
+});
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::truncate();
+        factory('App\User', 10)->create();
+    }
+}
