f6e0b3736ce02b1e861d79fad5ab9519322de793
Appkr <juwonkim@me.com>
2015-11-10 21:44:09 +0900

    18-eloquent-relationships

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public $timestamps = false;
+//    public $timestamps = false;
 
     protected $fillable = ['title', 'body'];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    public function posts() {
+        return $this->hasMany('App\Post');
+    }
 }
