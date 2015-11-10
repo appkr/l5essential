db27929ac12f1a8757f5af6fe5bb581caf4b2b20
Appkr <juwonkim@me.com>
2015-11-10 21:12:09 +0900

    17-authentication-201

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,25 @@
+@extends('master')
+
+@section('content')
+    <form method="POST" action="/auth/login">
+        {!! csrf_field() !!}
+
+        <div>
+            Email
+            <input type="email" name="email" value="{{ old('email') }}">
+        </div>
+
+        <div>
+            Password
+            <input type="password" name="password" id="password">
+        </div>
+
+        <div>
+            <input type="checkbox" name="remember"> Remember Me
+        </div>
+
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+@stop
