b80d05a5b981c66d2775525fc388e0852e8ad377
Appkr <juwonkim@me.com>
2015-11-10 17:23:13 +0900

    12-controller

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index() {
+        return view('index');
+    }
+}
