e503ca9848cbc54e272b890ed06632678fc11bc2
Appkr <juwonkim@me.com>
2015-11-10 15:35:40 +0900

    5-pass-data-to-view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,18 +1,33 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
+// Data binding using with() method
 Route::get('/', function () {
-    return view('welcome');
-//    return 'Hello World';
-//    return view('errors.503');
+    $greeting = 'Hello';
+
+    return view('index')->with('greeting', $greeting);
 });
+
+//// Binding more than two variables
+//Route::get('/', function () {
+//    return view('index')->with([
+//        'greeting' => 'Good morning ^^/',
+//        'name'     => 'Appkr'
+//    ]);
+//});
+//
+//// Binding data as a second argument of view() helper
+//Route::get('/', function () {
+//    return view('index', [
+//        'greeting' => 'Ola~',
+//        'name'     => 'Laravelians'
+//    ]);
+//});
+//
+//// Binding data as a property of View instance
+//Route::get('/', function () {
+//    $view = view('index');
+//    $view->greeting = "Hey~ What's up";
+//    $view->name = 'everyone';
+//
+//    return $view;
+//});
