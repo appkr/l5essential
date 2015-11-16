61bb7ffcf8980be3ef2c698b69563efb3cbf81aa
Appkr <juwonkim@me.com>
2015-11-16 22:23:36 +0900

    29-elixir

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,8 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <title>Laravel 5 Essential</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
     @yield('style')
 </head>
 <body>
@@ -13,7 +16,8 @@
         @include('footer')
     </div>
 
-        @yield('script')
+    <script src=" {{ elixir('js/app.js') }}"></script>
+    @yield('script')
 
 </body>
 </html>
