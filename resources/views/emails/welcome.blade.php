0e22d3c9d8195290f2652c244c3846f1db2fa2f4
Appkr <juwonkim@me.com>
2015-11-18 01:16:12 +0900

    clean up unused files and code blocks

--- resources/views/emails/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-<h1>{{ $title }} {{ $user->name }}</h1>
-<hr/>
-<p>{{ $body }}</p>
-<hr/>
-<footer>Mail from {{ config('app.url') }}</footer>
--- resources/views/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<hr>
-
-<footer>
-    <p class="text-center">This is footer</p>
-</footer>
