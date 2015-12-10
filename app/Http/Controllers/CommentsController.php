1fae83435c9acfa8d89f3566eba23ed9e3f4afa3
Appkr <juwonkim@me.com>
2015-12-11 02:48:30 +0900

    fix bug

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -11,7 +11,7 @@ class CommentsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('author:comment', ['except' => ['store']]);
+        $this->middleware('author:comment', ['except' => ['store', 'vote']]);
     }
 
     /**
